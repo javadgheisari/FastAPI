9947658d97829baac2ac75a4111feae0306be4d2
javad <javad.gheisarii@gmail.com>
2022-08-28 23:25:14 +0430

    user model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from typing import Union
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-
+from fastapi import FastAPI, Query, status
+from pydantic import BaseModel, EmailStr
 app = FastAPI()
 
 class Item(BaseModel):
@@ -9,6 +8,14 @@ class Item(BaseModel):
     price: int
     is_offer: Union[bool, None] = None
 
+class UserIn(BaseModel):
+    username : str
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    username: str
+    email: str
 
 
 @app.get("/")
@@ -25,6 +32,10 @@ def read_item(item_id: int, q: Union[str, None] = Query(None, max_length=32)):
 def update_item(item_id: int, item: Item):
     return {"item_id": item_id, "name": item.name, "price": item.price}
 
-@app.post("items/create")
+@app.post("items/create", status_code=status.HTTP_201_CREATED)
 def post_item(item: Item):
     return f'{item.name} is {item.price} $'
+
+@app.post('/user', response_model=UserOut)
+def user(user: UserIn):
+    return user
